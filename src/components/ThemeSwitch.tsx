7b510b55f4a903ba3065ec00f3b2f8ae5668afa3
Jesse <jessepena@hotmail.es>
2024-02-26 14:08:25 -0400

    Adds styling that modifies themeSwitch and mobile menu

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,18 +1,18 @@
 import { TiWeatherSunny } from "react-icons/ti";
 
 type Props = {
-	handleTheme: () => void;
-	darkThemeActivated: boolean;
+  handleTheme: () => void;
+  darkThemeActivated: boolean;
 };
 
 const ThemeSwitch = ({ handleTheme, darkThemeActivated }: Props) => {
-	return (
-		<TiWeatherSunny
-			size={20}
-			className="cursor-pointer fill-background-dark dark:fill-background hover:fill-primary"
-			onClick={handleTheme}
-		/>
-	);
+  return (
+    <TiWeatherSunny
+      size={20}
+      className="cursor-pointer fill-background-dark dark:fill-background hover:fill-primary hover:dark:fill-yellow"
+      onClick={handleTheme}
+    />
+  );
 };
 
 export default ThemeSwitch;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,38 +1,39 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-	darkMode: "selector",
-	content: [
-		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-	],
-	theme: {
-		extend: {
-			screens: {
-				"2xs": "320px",
-				xs: "375px",
-				sm: "425px",
-				md: "768px",
-				lg: "1024px",
-				xl: "1440px",
-				"2xl": "1536px",
-				"3xl": "2560px",
-			},
-			colors: {
-				primary: "#3474F3",
-				secondary: "#E3E8ED",
-				background: "#F7F3F3",
-				"text-primary": "#111827",
-				"text-secondary": "#898989",
-				"primary-dark": "#3474F3",
-				"secondary-dark": "#172135",
-				"background-dark": "#111827",
-				"text-primary-dark": "#F7F3F3",
-				"text-secondary-dark": "#ABADB2",
-			},
-		},
-	},
-	plugins: [],
+  darkMode: "selector",
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        "2xs": "320px",
+        xs: "375px",
+        sm: "425px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1440px",
+        "2xl": "1536px",
+        "3xl": "2560px",
+      },
+      colors: {
+        primary: "#3474F3",
+        secondary: "#E3E8ED",
+        background: "#F7F3F3",
+        "text-primary": "#111827",
+        "text-secondary": "#898989",
+        "primary-dark": "#3474F3",
+        "secondary-dark": "#172135",
+        "background-dark": "#111827",
+        "text-primary-dark": "#F7F3F3",
+        "text-secondary-dark": "#ABADB2",
+        yellow: "#FFD700",
+      },
+    },
+  },
+  plugins: [],
 };
 export default config;
